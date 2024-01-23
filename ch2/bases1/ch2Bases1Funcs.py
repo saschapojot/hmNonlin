e541a18aa2348dfa62dd4e317ea2980dc008d6c1
Xi LIU <xi.liu@u.nus.edu>
2024-01-23 20:34:47 +0800

    bases1 bases2

---
+++ ch2/bases1/ch2Bases1Funcs.py
@@ -0,0 +1,76 @@
+import numpy as np
+from sympy import *
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+
+#this script computes the Vm terms
+h=symbols("h",cls=Symbol,real=True)
+
+tau=symbols("tau",cls=Symbol)
+
+t=symbols("t",cls=Symbol)
+
+V0Func=tau
+
+V0Val=V0Func.subs(tau,t)
+
+def chi(m):
+    if m==0 or m==1:
+        return 0
+    if m >=2:
+        return 1
+
+VFuncsAll=[V0Func]#variable is tau
+RFuncsAll=[0]#variable is tau
+VValsAll=[V0Val]
+tSymbolStart=datetime.now()
+mEnd=10
+for m in range(1,mEnd+1):
+    RmFunc=diff(VFuncsAll[m-1],tau)
+    for j in range(0,m):
+        RmFunc+=VFuncsAll[j]*VFuncsAll[m-1-j]
+    RmFunc-=(1-chi(m))
+    RFuncsAll.append(RmFunc)
+    Vm=chi(m)*VValsAll[m-1]+h*integrate(RmFunc,(tau,0,t))
+    VValsAll.append(Vm)
+    VFuncsAll.append(Vm.subs(t,tau))
+tSymbolEnd=datetime.now()
+print("symbolic computation time: ",tSymbolEnd-tSymbolStart)
+def combineSameOrders(m):
+    """
+
+    :param m: order of Vm
+    :return:
+    """
+    Vm=VValsAll[m]
+    maxOrder=2*m+1
+    outExpression=0
+    for j in range(1,maxOrder+1):
+        coef=expand(Vm).coeff(t**j)
+        coef=factor(coef)
+        # pprint("j="+str(j)+", coef="+str(coef))
+        outExpression+=coef*t**j
+    return outExpression
+
+
+VTruncated=sum(VValsAll)
+
+
+V=lambdify([t,h],VTruncated,"numpy")
+
+hValsAll=[-1,-1/2,-1/10]
+colors=["black","lightcoral","forestgreen","darkslategrey","navy"]
+
+tValsAll=np.linspace(0,1.5,100)
+plt.figure()
+
+for i in range(0,len(hValsAll)):
+    est=[V(tVal,hValsAll[i]) for tVal in tValsAll]
+    # print(est)
+    plt.plot(tValsAll,est,c=colors[i])
+
+plt.scatter(tValsAll,np.tanh(tValsAll),color="purple",s=6)
+
+plt.savefig("ch2Fig2.1.png")
--- ch2/bases2/ch2Bases2Funcs.py
+++ ch2/bases2/ch2Bases2Funcs.py
@@ -2,72 +2,67 @@ import numpy as np
 from sympy import *
 from datetime import datetime
 import matplotlib.pyplot as plt
-#this script computes the Vm terms
+from scipy.misc import derivative
+#this script computes the Vm terms for bases 1/(1+t)^{m}
+
 h=symbols("h",cls=Symbol,real=True)
 
 tau=symbols("tau",cls=Symbol)
 
 t=symbols("t",cls=Symbol)
 
-V0Func=tau
-
+V0Func=1-1/(1+tau)
 V0Val=V0Func.subs(tau,t)
-
+H=1/(1+tau)
 def chi(m):
     if m==0 or m==1:
         return 0
     if m >=2:
         return 1
-
 VFuncsAll=[V0Func]#variable is tau
 RFuncsAll=[0]#variable is tau
 VValsAll=[V0Val]
 tSymbolStart=datetime.now()
-mEnd=10
+mEnd=20
 for m in range(1,mEnd+1):
     RmFunc=diff(VFuncsAll[m-1],tau)
     for j in range(0,m):
         RmFunc+=VFuncsAll[j]*VFuncsAll[m-1-j]
     RmFunc-=(1-chi(m))
     RFuncsAll.append(RmFunc)
-    Vm=chi(m)*VValsAll[m-1]+h*integrate(RmFunc,(tau,0,t))
+    Vm=chi(m)*VValsAll[m-1]+h*integrate(RmFunc*H,(tau,0,t))/(1+t)
     VValsAll.append(Vm)
     VFuncsAll.append(Vm.subs(t,tau))
 tSymbolEnd=datetime.now()
+
 print("symbolic computation time: ",tSymbolEnd-tSymbolStart)
-def combineSameOrders(m):
-    """
 
-    :param m: order of Vm
-    :return:
-    """
-    Vm=VValsAll[m]
-    maxOrder=2*m+1
-    outExpression=0
-    for j in range(1,maxOrder+1):
-        coef=expand(Vm).coeff(t**j)
-        coef=factor(coef)
-        # pprint("j="+str(j)+", coef="+str(coef))
-        outExpression+=coef*t**j
-    return outExpression
+#use apart() to do partial  fraction expansion
 
-
+# pprint(apart(VValsAll[1],t))#
 VTruncated=sum(VValsAll)
-
 
 V=lambdify([t,h],VTruncated,"numpy")
 
-hValsAll=[-1,-1/2,-1/10]
-colors=["black","lightcoral","forestgreen","darkslategrey","navy"]
-
-tValsAll=np.linspace(0,1.5,100)
+hValsAll=np.linspace(-1.9,-0.1,20)
+colors=["black","lightcoral","forestgreen","darkslategrey","navy","skyblue"]
+def Vat0(h):
+    return V(0,h)
+Vdd0=[derivative(Vat0,hTmp,dx=1e-6,n=2) for hTmp in hValsAll]
+Vddd0=[derivative(Vat0,hTmp,dx=1e-6,n=3) for hTmp in hValsAll]
 plt.figure()
 
-for i in range(0,len(hValsAll)):
-    est=[V(tVal,hValsAll[i]) for tVal in tValsAll]
-    # print(est)
-    plt.plot(tValsAll,est,c=colors[i])
+plt.plot(hValsAll,Vdd0,color="red",label="$V^{''}(0)$")
+plt.plot(hValsAll,Vddd0,color="blue",label="$V^{'''}(0)$")
 
-plt.scatter(tValsAll,np.tanh(tValsAll),color="purple",s=6)
+plt.xlabel("$\hbar$")
+plt.legend(loc="best")
+plt.savefig("bases2hcurve.png")
 
-plt.savefig("ch2Fig2.1.png")
+
+
+
+
+plt.figure()
+plt.plot(hValsAll,Vdd0,color="red",label="$V^{''}$")
+
